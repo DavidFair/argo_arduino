59075127279fb0067ecb53f61906b79f17668eda
David Fairbrother <DavidFair@users.noreply.github.com>
2018-03-04 23:13:43 +0000

    Fixed linker not picking up encoder lib

--- src/argo_rc/argo_rc.cpp
+++ src/argo_rc/argo_rc.cpp
@@ -3,13 +3,21 @@
 
 #include "arduino_hardware.hpp"
 #include "argo_rc_lib.hpp"
+#include "move.hpp"
+#include "unique_ptr.hpp"
 
 // Instantiate argo_rc library at the global level so it doesn't drop out of
 // scope
 // The hardware abstraction must live as long as the program runs so there
 // is no corresponding free
-Hardware::ArduinoInterface *hardwareImpl = new Hardware::ArduinoHardware();
-ArgoRcLib::ArgoRc argoRcLib(hardwareImpl);
+Argo::unique_ptr<Hardware::ArduinoInterface>
+    hardwareImpl(new Hardware::ArduinoHardware());
+
+Argo::unique_ptr<ArgoRcLib::ArgoEncoder>
+    encoderFactoryImpl(new ArgoRcLib::ArgoEncoder(*hardwareImpl));
+
+ArgoRcLib::ArgoRc argoRcLib(Argo::move(hardwareImpl),
+                            Argo::move(encoderFactoryImpl));
 
 void setup() { argoRcLib.setup(); }
 
