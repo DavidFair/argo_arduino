4ff01f8439494e92048823161f2e25220d9619d0
David Fairbrother <DavidFair@users.noreply.github.com>
2018-03-15 04:13:33 +0000

    Revert making unique_ptr constexpr

--- src/lib_wrappers/unique_ptr.hpp
+++ src/lib_wrappers/unique_ptr.hpp
@@ -5,7 +5,7 @@ namespace Libs {
 
 template <typename T> class unique_ptr {
 public:
-  constexpr unique_ptr(T *passedPtr) : ptr(passedPtr) {}
+  unique_ptr(T *passedPtr) : ptr(passedPtr) {}
   ~unique_ptr() { delete ptr; }
 
   // Copy
@@ -13,7 +13,7 @@ public:
   unique_ptr &operator=(const unique_ptr &a) = delete;
 
   // Move
-  constexpr unique_ptr(unique_ptr &&other) {
+  unique_ptr(unique_ptr &&other) {
     ptr = other.ptr;
     other.ptr = nullptr;
   }
@@ -26,22 +26,22 @@ public:
     return *this;
   }
 
-  constexpr T *get() const { return ptr; }
+  T *get() const { return ptr; }
 
-  constexpr void reset(T *newPtr) {
+  void reset(T *newPtr) {
     delete ptr;
     ptr = newPtr;
   }
 
-  constexpr T *release() {
+  T *release() {
     T *ptrToReturn = ptr;
     ptr = nullptr;
     return ptrToReturn;
   }
 
-  constexpr T &operator*() { return *ptr; }
+  T &operator*() { return *ptr; }
 
-  constexpr T *operator->() { return ptr; }
+  T *operator->() { return ptr; }
 
 private:
   T *ptr;
